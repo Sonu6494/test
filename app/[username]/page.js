072da353955d29e07c7432e2a00ec74a8e5248b2
sonu kumar <sonukumar2323sonu@gmail.com>
2024-11-30 19:29:51 +0530

    Initial Commit

---
+++ app/[username]/page.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PaymentPage from '@/components/PaymentPage'
+import { notFound } from "next/navigation"
+import connectDb from '@/db/connectDb'
+import User from '@/models/User'
+const Username = async ({ params }) => {
+  const { username } = await params;
+
+  // If the username is not present in the database, show a 404 page
+  const checkUser = async () => {
+    await connectDb()
+    let u = await User.findOne({ username })
+    if (!u) {
+      return notFound()
+    }
+  };
+  await checkUser()
+
+  // const { username } = await params;
+  return (
+    <>
+      <PaymentPage username={username} />
+    </>
+  )
+}
+
+export default Username
+
+export async function generateMetadata({ params }) {
+  const { username } = await params;
+  return {
+    title: `Support ${username} - Get me a Chai`,
+  }
+}
